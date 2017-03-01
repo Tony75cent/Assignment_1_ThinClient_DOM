9b8922d194dc6784cbf4421d94c44586164f8812
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-03-01 22:15:02 +0000

    Dropped the CSS  geneology Tree(The Code Player) and replaced with a much simpler <ul><li></li></ul> list. Much more work to do.

--- public_html/JS/nodeHighlighter.js
+++ public_html/JS/nodeHighlighter.js
@@ -5,51 +5,43 @@
  */
 
 $(document).ready(function () {
-    $(".listitem a").first().addClass("highLight"); 
+     $("li:eq(0)").first().addClass("highLight");  
 
-    $(".listitem a ").on("mouseover", addHighlight);
-    $("#btn1").on("click", moveDownDom);
-   /**  $("#btn2").on("click", addChild); **/
-
-    $("a").on("mouseout", removeHighlight);
-     $(".listitem a").on("click",addChild); 
+  /**  $(".listitem li ").on("mouseover", addHighlight); **/
+   
+  /**  $("li").on("mouseout", removeHighlight **/
+    $(".listitem li").on("click", addChild);
     function removeHighlight() {
         $(this).removeClass("highLight");
     }
     function addHighlight() {
         $(this).addClass("highLight");
     }
-    function addChild() {  
-        var $p = prompt("Enter new name", "");
- /** var $currItem = $(".listitem a").filter(".highLight"); **/
- /** var $currItem = $("listitem a").filter(".highLight"); **/
- /**  $(this).add("li").text($p); **/
-  $(".listitem a").filter(".highLight").next().append("<li>"+$p+"<li>");
-
- 
-
-          /** $currItem.append("li a").text(p);  **/
-                    $currItem.add(" li").text($p);
-/**  $("<li>"+$p+"</li>").appendTo(this); **/
-       /** $currItem.this.append("li").text(p); **/
-
-        /** $(this).append("li").text(p); **/
-        /**   $(this).filter(".highLight").add("li").text(p); **/
+    function addChild() {
+        $(this).addClass("highLight");
+        var $tmp = prompt("Enter new name", "");
+        $(".listitem li").filter(".highLight");
+        var $pos = $(this);
+        $(this).append("<li>" + $tmp + "</li>",{"class":"active"});
+        $(this).removeClass("highLight");
     }
 });
 function moveDownDom() {
-    var $current = $(".listitem a").filter(".highLight");
+    var $current = $(".listitem li").filter(".highLight");
     /** $(this).add("li").text("Test@21:05"") **/
     $current.add("li");
 
-    /**  var $listItems =   $(".listitem a"); **/
-    /**alert("Test@20:03"); **/
-    /**   var $currentSelectedItem = $listItems.filter(".highLight");
-     /**    var $name = $currentSelectedItem.text(); **/
-    /** $(".tree  .listitem ul").append("li").text("test@19:55"); **/
-    /**  $("#msg").text($name); **/
-    /**    $currentSelectedItem.removeClass("highLight"); **/
-    /**    $currentSelectedItem.first().addClass("highLight");  **/
+
+}
+
+function createNode($arg1, $arg2) {
+    var $li = $(this).append("<li>" + $arg1 + "</li>",{"class":"tree li a "});
+    var $ele = $arg2;
+    
+
+
+
+
 
 
 }
