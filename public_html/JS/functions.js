24455e8ba59e2d6c408f55d9f543428f2d7bb38a
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-03-05 21:25:46 +0000

    Added more functionality.Added Remove Child,Change Text,Add new child, Add sibling. The Add sibling is only partially working.

--- public_html/JS/functions.js
+++ public_html/JS/functions.js
@@ -13,27 +13,33 @@ $(function () {
     var $tmp;
     var $cur;
     var $currentElem;
+    var $current;/** The DOM  element which has the highLight class attached to it. **/
     $("#moveDownDom").on("click", moveDownDOM);
     $("#moveUpDom").on("click", moveUpDOM);
     $("#addNewChild").on("click", addNewChild);
     $("#changeText").on("click", changeText);
+    $("#removeChild").on("click", removeChild);
+    $("#addSibling").on("click", addSibling);
+
+
+
 
 
 
 
 
     function moveDownDOM() {
-        /** var **/ $theList = $('li');
-        /**  var **/ $currentElem = $('.highLight').last();
+        $theList = $('li');
+        $currentElem = $('.highLight').last();
 
 
-        /** var **/ pos = $theList.index($currentElem);
+        pos = $theList.index($currentElem);
         pos++;
         $currentElem.removeClass('highLight');
         if ($theList.eq(pos).children('li').length > 0)
         {
-            /**  var  **/$tmp = $currentElem.children("li:eq(0)");
-            /**  var **/ $cur = $tmp.parent(this);
+            $tmp = $currentElem.children("li:eq(0)");
+            $cur = $tmp.parent(this);
             $cur.addClass("highLight");
         } else
             $theList.eq(pos).addClass('highLight');
@@ -41,40 +47,65 @@ $(function () {
     ;
 
     function moveUpDOM() {
-        /** var $theList = $('li'); **/
-        /**  var **/ $currentElem = $('.highLight').last();
+        $currentElem = $('.highLight').last();
 
 
-        /** var  **/pos = $theList.index($currentElem);
+        pos = $theList.index($currentElem);
         pos--;
         $currentElem.removeClass('highLight');
         if ($theList.eq(pos).children('li').length > 0)
         {
-            /** var **/ $tmp = $currentElem.children("li:eq(0)");
-            /** var **/ $cur = $tmp.parent(this);
+            $tmp = $currentElem.children("li:eq(0)");
+            $cur = $tmp.parent(this);
             $cur.addClass("highLight");
         } else
             $theList.eq(pos).addClass('highLight');
     }
 
-    function addNewChild() {
 
-        var $newName = prompt("Enter name of child", "");
-        $currentElem.append("<li>" + $newName + "</li>");
-        /**  $("li").append($newName).after().filter('.highLight'); **/
-        /**  $("li").append($newName).after().filter('.highLight'); **/
-        /** $currentElem.prepend("<li>" + $newName + "</li>");**/
-        /**$currentElem.append("<li>" + $newName + "</li>"); **/
-        /** $currentElem.children().append("<li>" + $newName + "</li>"); **/
+
+
+    function changeText() {
+        var $newName = prompt("Enter the new name", "");
+        $current = $("li").filter(".highLight"); /**The global variable $current is referenced to the 
+         single li which has the claa highLight attached to it **/
+        $current.text($newName);
+
+
+
+
 
 
     }
-    function changeText() {
+    function addNewChild() {
         var $newName = prompt("Enter the new name", "");
-        var $current = $(".listitem li").filter(".highLight");
-        $current.text($newName); /** This is good, very good, I have found a reliable way of selecting
-                the current element and it works. Complete the other bits demain **/
-        /** $currentElem.text($newName); **/
+        $current = $("li").filter(".highLight");
+        $current.append("<li>" + $newName + "</li>");
+        moveDownDOM();/**Invoke the moveDownDOM() function, so that the new li is referenced
+         *                        as the $current DOM  element  **/
+
+
+    }
+
+    function removeChild() {
+
+        $current = $("li").filter(".highLight");
+        $current.remove(".highLight");
+        moveDownDOM();
+    }
+    function addSibling() {
+        var $newName = prompt("Enter the new name", "");
+        $current = $("li").filter(".highLight");
+        /**  $($current).after("<li>" + $newName + "</li>"); **/
+        /**  $($current).next().append("<li>" + $newName + "</li>"); **/
+        if ($($current).is("li")) {
+
+            $($current).append("<ul><li>" + $newName + "</li></ul>");
+            moveDownDOM();
+
+        }
+      /**  elseIf **/
+
 
 
 
